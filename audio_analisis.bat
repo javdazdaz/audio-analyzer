ac56a28a1639f850c5387cf914c87ee093fac24b
Javier <javdazdaz@gmail.com>
2025-07-21 13:00:51 -0400

    Mejorar script para pato V2

--- audio_analisis.bat
+++ audio_analisis.bat
@@ -1,12 +1,15 @@
 @echo off
-REM Activar entorno virtual
-call venv\Scripts\activate.bat
 
-REM Ir a la carpeta app (opcional, para claridad)
-cd app
 
 REM Ejecutar la aplicación con python -m para resolver imports
-python -m main
+python -m app.main
+
+
+REM Esperar unos segundos para levantar el servidor antes de abrir navegador
+timeout /t 5 /nobreak >nul
+
+REM Abrir navegador en la URL de la app
+start "" http://127.0.0.1:8050/
 
 REM Pausar para que veas errores (opcional, quita si quieres que cierre rápido)
 pause
