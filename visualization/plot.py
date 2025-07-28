7c23f18ba689f68bb2cbdfd339f5dac23e18d255
Javier <javdazdaz@gmail.com>
2025-07-27 21:21:09 -0400

    Optimizar waveform y spectrograma a coste de presición

--- visualization/plot.py
+++ visualization/plot.py
@@ -2,30 +2,39 @@
 
 import numpy as np
 import plotly.graph_objs as go
-from scipy import signal
-from scipy.fft import rfft, rfftfreq
 import librosa
+from scipy.fft import rfft, rfftfreq
+from scipy import signal
 
-def generate_waveform(y, sr, time_range=None):
+def generate_waveform(y, sr, time_range=None, downsample_factor=10):
     """
-    Genera figura Plotly para waveform.
+    Genera figura Plotly para waveform, aplicando downsampling para acelerar.
+
     """
     if time_range is None:
-        time = np.arange(len(y)) / sr
-        data = y
+        y_segment = y
+        start_time = 0
     else:
-        start_idx = int(time_range[0] * sr)
-        end_idx = int(time_range[1] * sr)
-        time = np.arange(start_idx, end_idx) / sr
-        data = y[start_idx:end_idx]
+        start_sample = int(time_range[0] * sr)
+        end_sample = int(time_range[1] * sr)
+        y_segment = y[start_sample:end_sample]
+        start_time = time_range[0]
+
+    # Downsample para acelerar trazado y reducir cantidad de puntos
+    y_down = y_segment[::downsample_factor]
+    n_points = len(y_down)
+
+    # Tiempo con np.arange a paso downsample
+    time = np.arange(n_points) * downsample_factor / sr + start_time
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(
         x=time,
-        y=data,
+        y=y_down,
         fill='tozeroy',
         line=dict(color='rgb(40, 120, 181)')
     ))
+
     fig.update_layout(
         title='Waveform',
         xaxis_title='Time (s)',
@@ -34,39 +43,41 @@ def generate_waveform(y, sr, time_range=None):
         height=300,
         template='plotly_white'
     )
+
     return fig
 
-def generate_spectrogram(y, sr, time_range=None):
+def generate_spectrogram(y, sr, time_range=None, n_fft=512, hop_length=256):
     """
-    Genera figura Plotly para espectrograma basado en STFT de librosa.
+    Genera figura Plotly para espectrograma usando librosa.stft, hop_length para acelerar.
     """
-    if time_range:
-        start_idx = int(time_range[0] * sr)
-        end_idx = int(time_range[1] * sr)
-        y_segment = y[start_idx:end_idx]
-    else:
+    if time_range is None:
         y_segment = y
-
-    n_fft = 1024
-    hop_length = 512
+        start_time = 0
+    else:
+        start_sample = int(time_range[0] * sr)
+        end_sample = int(time_range[1] * sr)
+        y_segment = y[start_sample:end_sample]
+        start_time = time_range[0]
 
     S = np.abs(librosa.stft(y_segment, n_fft=n_fft, hop_length=hop_length))
     S_db = librosa.amplitude_to_db(S, ref=np.max)
+
     times = librosa.times_like(S_db, sr=sr, hop_length=hop_length, n_fft=n_fft)
-    if time_range:
-        times += time_range[0]
+    times += start_time
+
+    freqs = librosa.fft_frequencies(sr=sr, n_fft=n_fft)
 
     fig = go.Figure(data=go.Heatmap(
-        x=times,
-        y=librosa.fft_frequencies(sr=sr, n_fft=n_fft),
         z=S_db,
+        x=times,
+        y=freqs,
         colorscale='Viridis',
         zmin=-80,
-        zmax=0,
+        zmax=0
     ))
 
     fig.update_layout(
-        title='Spectrogram',
+        title=f'Spectrogram',
         xaxis_title='Time (s)',
         yaxis_title='Frequency (Hz)',
         margin=dict(l=20, r=20, t=40, b=20),
